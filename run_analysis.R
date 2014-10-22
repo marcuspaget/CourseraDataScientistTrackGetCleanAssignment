cd09cf1bc4b6be648675005026ba9699b7a28d12
Mark Paget <marcuspaget@gmail.com>
2014-10-22 21:10:46 +0800

    script to perform analysis

---
+++ run_analysis.R
@@ -0,0 +1,21 @@
+#......................................................................................................#
+# 
+#  Author: Mark Paget		Script Name: run_analysis.R   GitHub: http://github.org/marcuspaget
+#
+#  Date: 16th Oct 2014
+#
+#  Description:  This script is part of the Data Science course provided by John Hopkins School via coursera 
+#
+#  Requirements:
+#
+#  1.  Merges the training and the test sets to create one data set.
+#  2.  Extracts only the measurements on the mean and standard deviation for each measurement. 
+#  3.  Uses descriptive activity names to name the activities in the data set
+#  4.  Appropriately labels the data set with descriptive variable names. 
+#  5.  From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+#
+#  Ref: http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
+#
+#  Data Source: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
+#
+#  Data Source obtained at ....     Checksum ....  Modified Date:  Last-Modified: Wed, 15 Jan 2014 14:20:28 GMT
